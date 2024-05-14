a8a6925e8c80aafe56c784e84b92a9f6ce588967
Apelaiz <apelaiz@yandex.ru>
2024-05-14 15:53:44 +0300

    task-03

---
+++ Третий урок/task-03.js
@@ -0,0 +1,13 @@
+// titleProject (String)
+// ScreensValue (String)
+// screenPrice  (Number)
+// percentage   (Number)
+// fullPrice    (Number)
+// responsive   (Boolean)
+
+let titleProject = "Калькулятор"
+let ScreensValue = "Шаблоны, с уникальным дизайном, с анимациями"
+let screenPrice = 10000
+let percentage = 5
+let fullPrice = 50000
+let responsive = true
